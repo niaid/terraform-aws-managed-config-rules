f9bd208603e352d1c20700f885af5d746adb89ed
Carl Benson <bensonce@nih.gov>
2024-06-13 10:36:04 -0400

    Fixed an issue where the setting the rule identifier by modifying the rule name breaks rules whose names don't match their identifiers.

--- modules/organization/main.tf
+++ modules/organization/main.tf
@@ -2,17 +2,17 @@ resource "aws_config_organization_managed_rule" "rule" {
   for_each = var.rules
 
   name                 = "${var.rule_name_prefix}${each.key}"
-  rule_identifier      = upper(replace(each.key, "-", "_"))
+  rule_identifier      = each.value.identifier
   excluded_accounts    = var.excluded_accounts
-  description          = try(each.value["description"], "")
-  resource_types_scope = try(each.value["resource_types_scope"], [])
+  description          = try(each.value.description, "")
+  resource_types_scope = try(each.value.resource_types_scope, [])
 
   input_parameters = (
     # AWS Config expects all values as strings. This list comprehension
     # removes optional parameter attributes whose value is 'null'.
-    try(jsonencode(each.value["input_parameters"]), null) != "null" ?
+    try(jsonencode(each.value.input_parameters), null) != "null" ?
     try(jsonencode(
-      { for k, v in each.value["input_parameters"] :
+      { for k, v in each.value.input_parameters :
         k => tostring(v) if v != null }), null) :
     null
   )
