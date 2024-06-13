e064f4cd51d070784ba768957f81f83c45fa7a3d
Carl Benson <bensonce@nih.gov>
2024-06-13 11:36:28 -0400

    Revert map key changes

--- modules/account/main.tf
+++ modules/account/main.tf
@@ -2,23 +2,23 @@ resource "aws_config_config_rule" "rule" {
   for_each = var.rules
 
   name        = "${var.rule_name_prefix}${each.key}"
-  description = try(each.value.description, "")
+  description = try(each.value["description"], "")
 
   scope {
-    compliance_resource_types = try(each.value.resource_types_scope, [])
+    compliance_resource_types = try(each.value["resource_types_scope"], [])
   }
 
   source {
     owner             = "AWS"
-    source_identifier = each.value.identifier
+    source_identifier = each.value["identifier"]
   }
 
   input_parameters = (
     # AWS Config expects all values as strings. This list comprehension
     # removes optional parameter attributes whose value is 'null'.
-    try(jsonencode(each.value.input_parameters), null) != "null" ?
+    try(jsonencode(each.value["input_parameters"]), null) != "null" ?
     try(jsonencode(
-      { for k, v in each.value.input_parameters :
+      { for k, v in each.value["input_parameters"] :
         k => tostring(v) if v != null }), null) :
     null
   )
